5b2795e11e6a293aee4680944fa509065d03577b
trantuyet762 <trantuyet2203@gmail.com>
2024-04-11 01:37:18 +0700

    update11/4

---
+++ WebBanHangOnline/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.EF;
+namespace WebBanHangOnline.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Partial_ItemsByCateId()
+        {
+            var items = db.Products.Where(x => x.IsHome && x.IsActive).Take(12).ToList();
+            return PartialView(items);
+        }
+        public ActionResult Partial_ProductSales()
+        {
+            var items = db.Products.Where(x => x.IsSale && x.IsActive).Take(12).ToList();
+            return PartialView(items);
+        }
+    }
+}
